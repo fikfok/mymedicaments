6312495b31ebfc8e2d03f924b48c8f1940c625a0
sadovenko <sadovenko.dmitry@gmail.com>
2018-10-14 22:04:29 +0300

    login/logout

--- mymedicaments/urls.py
+++ mymedicaments/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from mymedicaments import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path(r'^login/$', LoginView.as_view(template_name='registration/login.html'), name="login"),
+    url(r'^logout/$', LogoutView.as_view(template_name='registration/logout.html'), name="logout"),
     url(r'^$', views.home, name='home'),
 ]
