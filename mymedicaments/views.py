d6a54f454aa6d9aba5ef8b0d17ae3905ce0201f9
sadovenko <sadovenko.dmitry@gmail.com>
2018-10-24 23:33:27 +0300

    modal create

--- mymedicaments/views.py
+++ mymedicaments/views.py
@@ -12,12 +12,13 @@ def get_medicaments(request):
     base_url = request.build_absolute_uri().replace(request.get_full_path(), '')
 
     medicaments = Medicament.objects.filter(author=request.user)
-    data = [
+
+    data = {'data': [
         [
             item.name,
             item.price,
             base_url + item.photo.url
         ]
         for item in medicaments
-    ]
+    ]}
     return JsonResponse(data, safe=False)
