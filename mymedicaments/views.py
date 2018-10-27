796bafc36b8dd4d9184f940d0d67797ee4a2d16c
sadovenko <sadovenko.dmitry@gmail.com>
2018-10-28 00:09:19 +0300

    save

--- mymedicaments/views.py
+++ mymedicaments/views.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 
 from django.conf import settings
@@ -33,10 +34,20 @@ def get_medicaments(request):
 def save_medicament(request):
     if request.method == 'POST':
         path = settings.MEDIA_ROOT + '/'
-        file_name = uuid.uuid4()
-        form = MedicamentForm(request.POST, request.FILES)
+        file_name = str(uuid.uuid4())
+
+        photo_file = request.FILES['photo']
+        path = path + '/'
+        ext = os.path.splitext(photo_file.name)[-1].lower()
+        absolut_path = path + file_name + ext
+        destination = open(absolut_path, 'wb+')
+        for chunk in photo_file.chunks():
+            destination.write(chunk)
+        destination.close()
+
+        form = MedicamentForm(request.POST)
         medicament = form.save(commit=False)
+        medicament.photo = file_name + ext
         medicament.author = request.user
-        # form.fields['author'] = request.user
         medicament.save()
     return JsonResponse({}, safe=False)
